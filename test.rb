31b3bf72e7e3d0b54e15fc597966dd8c16adfd7b
github username <github email address>
2020-04-23 13:58:52 -0400

    first commit

---
+++ test.rb
@@ -0,0 +1,15 @@
+# git workflow 
+ 
+# make new repoo in github
+# link repo to code 
+# add collaborator 
+# collaborator accepts request and clones the code - do not fork
+
+# only one person works oon the master at a given time
+# when that persons turn is up they:
+# git add . 
+# git coommit -m "these are the changes that were made"
+# git push 
+
+# the other person then pulls down the coode with "git pull" 
+# that workflow continues 
